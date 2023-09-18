30e9af6c0b356c0c475cae480acb09d83d746be7
plamenna-petrova <plamennavp@abv.bg>
2023-09-18 17:52:10 +0300

    Added the console MVC application for the Histogram task

---
+++ src/Exercises/Three-Layer-Model-Review/HistogramConsoleMVC/Controllers/HistogramController.cs
@@ -0,0 +1,23 @@
+﻿using HistogramConsoleMVC.Models;
+using HistogramConsoleMVC.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HistogramConsoleMVC.Controllers
+{
+    public class HistogramController
+    {
+        private HistogramEntriesInfo numbersInfo;
+
+        private Display display;
+
+        public HistogramController()
+        {
+            display = new Display();
+            numbersInfo = new HistogramEntriesInfo(display.HistogramEntries);
+            display.HistogramPartitionPercentages = numbersInfo.GetHistogramPercentageEntries();
+            display.ShowHistogramEntriesPartitionPercentages();
+        }
+    }
+}
